0e87e58ac419d53d1c667b27df1041712e7a8a5a
Ezalor1337 <alexkalugin532@gmail.com>
2023-11-06 15:10:23 +0300

    Beans were created, please check the app

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,17 +1,30 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private Animal dog;
+    private Animal cat;
+    private Timer timer;
+
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal dog, @Qualifier("cat") Animal cat, Timer timer) {
+        this.dog = dog;
+        this.cat = cat;
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(dog.toString() + ", " + cat.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
